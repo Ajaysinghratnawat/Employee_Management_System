09f4d80a4aed29b79610edee7e1b962b7d5da469
Ajay singh ratnawat <168153835+Ajaysinghratnawat@users.noreply.github.com>
2026-01-13 09:22:23 +0530

    Employee Management System

---
+++ Employee-Management-System/src/main/java/com/ems/controller/RegisterServlet.java
@@ -0,0 +1,62 @@
+package com.ems.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//Accept the data
+		String id = request.getParameter("id");
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String salary = request.getParameter("salary");
+		String dept = request.getParameter("dept");
+		
+		String URL="jdbc:mysql://localhost:3306/ems";
+		String username = "root";
+		String userpass="Ajay@9977";
+		//Database (JDBC)
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con= DriverManager.getConnection(URL,username,userpass);
+			PreparedStatement pstmt = con.prepareStatement("Insert into employee values(?,?,?,?,?)");
+			pstmt.setString(1, id);
+			pstmt.setString(2, name);
+			pstmt.setString(3, email);
+			pstmt.setString(4, salary);
+			pstmt.setString(5, dept);
+			int rowsaffected = pstmt.executeUpdate();
+			if(rowsaffected!=0) {
+				System.out.println("Successfully inserted....");
+				RequestDispatcher rd = request.getRequestDispatcher("/Home.jsp");
+				rd.forward(request, response);
+//				out.println("<h2 style='color:green;'>Successfully Registered</h2>");
+			}
+			else {
+				System.out.println("Unsuccessful");
+				out.print("Try Again");
+				RequestDispatcher rd = request.getRequestDispatcher("Registration.html");
+				rd.include(request, response);
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
